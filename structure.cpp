9e9af9e8440effcc1c4dba22a974a0c501669272
Badhon <example.email>
2021-11-17 21:52:12 +0600

    addded structure and got 3rd star :)

---
+++ structure.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+typedef struct 
+{
+    string first,second;
+    int num,numm;
+}info;
+
+int main()
+{
+    info demo;
+    cin >> demo.num >> demo.first >> demo.second >> demo.numm;
+    cout << demo.num << " "<< demo.first<<" "<<demo.second << " "<<demo.numm;
+   
+
+    
+
+
+
+}
